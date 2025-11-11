17c7c9e3afb9bb1920734f12b2a885289fe484d3
Igor Davydov <bzza@Igors-MacBook-Air.local>
2025-11-11 18:38:42 +0300

    Build.gradle corrected

---
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -0,0 +1,42 @@
+package ru.netology.data;
+
+import lombok.SneakyThrows;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLHelper {
+    private static final QueryRunner QUERY_RUNNER = new QueryRunner();
+
+    private SQLHelper() {
+    }
+
+    private static Connection getConn() throws SQLException {
+        return DriverManager.getConnection(System.getProperty("db.url"), "app", "pass");
+    }
+
+    @SneakyThrows
+    public static DataHelper.VerificationCode getVerificationCode() {
+        var codeSQL = "SELECT code FROM auth_codes ORDER BY created DESC LIMIT 1";
+        var conn = getConn();
+        return QUERY_RUNNER.query(conn, codeSQL, new BeanHandler<>(DataHelper.VerificationCode.class));
+    }
+
+    @SneakyThrows
+    public static void cleanDatabase() {
+        var connection = getConn();
+        QUERY_RUNNER.execute(connection, "DELETE FROM auth_codes");
+        QUERY_RUNNER.execute(connection, "DELETE FROM card_transactions");
+        QUERY_RUNNER.execute(connection, "DELETE FROM cards");
+        QUERY_RUNNER.execute(connection, "DELETE FROM users");
+    }
+
+    @SneakyThrows
+    public static void cleanAuthCodes() {
+        var connection = getConn();
+        QUERY_RUNNER.execute(connection, "DELETE FROM auth_codes");
+    }
+}
---
+++ src/test/java/ru/netology/test/BankLoginTest.java
@@ -0,0 +1,77 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.*;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+
+
+//import static com.codeborne.selenide.Selenide;
+import static java.nio.channels.Selector.open;
+import static ru.netology.data.SQLHelper.cleanAuthCodes;
+import static ru.netology.data.SQLHelper.cleanDatabase;
+
+class BankLoginTest {
+
+    LoginPage loginPage;
+
+    @AfterEach
+    void tearDown() {
+        cleanAuthCodes();
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        cleanDatabase();
+    }
+
+    @BeforeEach
+    void setUp() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    private LoginPage open(String url, Class<LoginPage> loginPageClass) {
+        return null;
+    }
+
+    @Test
+    @DisplayName("Позитивный тест")
+    void shouldSuccessfulLogin() {
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    @DisplayName("Неверно указано имя пользователя")
+    void shouldErrorInvalideLogin() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! Неверно указан логин или пароль");
+    }
+
+    @Test
+    @DisplayName("Неверно указан код верификации")
+    void shouldInvalidCodes() {
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotification("Ошибка! Неверно указан код! Попробуйте ещё раз.");
+    }
+
+    @Test
+    @DisplayName("Ввод неправильного пароля 3 раза")
+    void shouldLockAfterThreeUnsuccessfulPasswords() {
+        var authInfo = DataHelper.generateRandomUser();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyErrorNotification("Ошибка! Неверно указан логин или пароль");
+        for (int count = 0; count < 3; count++) {
+            loginPage.validLogin(DataHelper.generateRandomUser());
+        }
+        verificationPage.verifyErrorNotification("Превышено максимальное количество попыток авторизации");
+    }
+}
